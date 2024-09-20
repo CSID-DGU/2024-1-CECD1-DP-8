4da267b4a8f0653560fdca76b502067623ef0cfe
Limwngur <ovg07047@naver.com>
2024-09-20 22:18:23 +0900

    feat: Influencer_id 존재 여부 validator 어노테이션 구현 - @IsExistInfluencer 을 구현하였고, ReportController의 Parameter 요소에 적용하였습니다.

---
+++ dp-server/src/main/java/com/cecd/dp/validation/InfluencerExistValidator.java
@@ -0,0 +1,34 @@
+package com.cecd.dp.validation;
+
+import com.cecd.dp.domain.influencer.repository.InfluencerRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class InfluencerExistValidator implements ConstraintValidator<IsExistInfluencer, Long> {
+
+    private InfluencerRepository influencerRepository;
+
+    public InfluencerExistValidator(InfluencerRepository influencerRepository) {
+        this.influencerRepository = influencerRepository;
+    }
+
+    @Override
+    public void initialize(IsExistInfluencer constraintAnnotation) {}
+
+    @Override
+    public boolean isValid(Long influencerId, ConstraintValidatorContext context) {
+        if (influencerId == null) {
+            return true;
+        }
+
+        boolean isValid = influencerRepository.findById(influencerId).isPresent();
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context
+                    .buildConstraintViolationWithTemplate("There is no exist influencerId : " + influencerId)
+                    .addConstraintViolation();
+
+        }
+        return isValid;
+    }
+}
