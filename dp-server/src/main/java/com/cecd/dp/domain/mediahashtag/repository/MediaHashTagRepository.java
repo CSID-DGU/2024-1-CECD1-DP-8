0c80e9dfab0c34899650beca929f90e2ab2d526a
jonghyuck <iqbox2@naver.com>
2024-09-21 14:23:33 +0900

    Fix : 커밋 이슈 수정

--- dp-server/src/main/java/com/cecd/dp/domain/mediahashtag/repository/MediaHashTagRepository.java
+++ dp-server/src/main/java/com/cecd/dp/domain/mediahashtag/repository/MediaHashTagRepository.java
@@ -3,5 +3,4 @@ package com.cecd.dp.domain.mediahashtag.repository;
 import com.cecd.dp.domain.mediahashtag.entity.MediaHashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MediaHashTagRepository extends JpaRepository<MediaHashTag, Long> {
-}
+public interface MediaHashTagRepository extends JpaRepository<MediaHashTag, Long> {}
