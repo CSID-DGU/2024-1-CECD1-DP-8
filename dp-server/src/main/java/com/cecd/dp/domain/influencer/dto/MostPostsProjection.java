d53cddcb1d2b10893b254f92d0f3d25695b8b35b
Limwngur <ovg07047@naver.com>
2024-09-27 14:48:33 +0900

    feat: 인플루언서 리포트 조회 구현 w/o 차트 그래프 - 그래프에 대한 분석 데이터는 어떻게 줄 지 한번 더 생각하고 구현한 뒤에 추가적으로 commit 할 예정

--- dp-server/src/main/java/com/cecd/dp/domain/influencer/dto/MostPostsProjection.java
+++ dp-server/src/main/java/com/cecd/dp/domain/influencer/dto/MostPostsProjection.java
@@ -1,4 +1,19 @@
 package com.cecd.dp.domain.influencer.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MostPostsProjection {
+
+    private String thumbnailUrl;
+    private Integer likeCnt;
+    private Integer commentsCnt;
+
+    public MostPostsProjection(String thumbnailUrl, Integer likeCnt, Integer commentsCnt) {
+        this.thumbnailUrl = thumbnailUrl;
+        this.likeCnt = likeCnt;
+        this.commentsCnt = commentsCnt;
+    }
 }
--- dp-server/src/main/java/com/cecd/dp/domain/influencer/repository/InfluencerRepository.java
+++ dp-server/src/main/java/com/cecd/dp/domain/influencer/repository/InfluencerRepository.java
@@ -1,6 +1,69 @@
 package com.cecd.dp.domain.influencer.repository;
 
+import com.cecd.dp.domain.influencer.dto.MostPostsProjection;
+import com.cecd.dp.domain.influencer.dto.ProfileProjection;
 import com.cecd.dp.domain.influencer.entity.Influencer;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface InfluencerRepository extends JpaRepository<Influencer, Long> {}
+import java.util.List;
+
+public interface InfluencerRepository extends JpaRepository<Influencer, Long> {
+
+    @Query(
+            "SELECT new com.cecd.dp.domain.influencer.dto.ProfileProjection(i.profilePictureUrl, i.nickname, i.name, i.category, SIZE(md), mt.followerCnt) "
+                    + "FROM Influencer i "
+                    + "JOIN i.mediaList md "
+                    + "JOIN i.metaList mt "
+                    + "WHERE i.id = :id "
+                    + "ORDER BY mt.createdAt DESC"
+    )
+    List<ProfileProjection> getProfileById(@Param("id") Long influencerId, Pageable pageable);
+
+    @Query(
+            "SELECT new com.cecd.dp.domain.influencer.dto.MostPostsProjection(md.thumbnailUrl, md.likeCnt, md.commentsCnt) "
+                    + "FROM Influencer i JOIN i.mediaList md "
+                    + "WHERE i.id = :id "
+                    + "ORDER BY (md.likeCnt + md.commentsCnt) DESC")
+    List<MostPostsProjection> getMostThreePostsById(@Param("id") Long influencerId, Pageable pageable);
+
+    @Query(
+            "SELECT DISTINCT ht.name "
+            + "FROM Influencer i "
+            + "JOIN i.mediaList md "
+            + "JOIN md.mediaHashTagList mht "
+            + "JOIN mht.hashTag ht "
+            + "WHERE i.id = :id"
+    )
+    List<String> getAllTagNamesById(@Param("id") Long influencerId);
+
+    @Query(
+            "SELECT (mt.likeAvg+mt.commentsAvg)/mt.followerCnt*100 "
+                    + "FROM Influencer i JOIN i.metaList mt "
+                    + "WHERE i.id = :id "
+                    + "ORDER BY mt.createdAt DESC"
+    )
+    List<Float> calculateReactionQuotientById(@Param("id") Long influencerId, Pageable pageable);
+
+    @Query(value = "SELECT AVG(subquery.like_cnt) "
+            + "FROM ( "
+            + "    SELECT m.like_cnt "
+            + "    FROM Media m "
+            + "    WHERE m.influencer_id = :id "
+            + "    ORDER BY m.created_at DESC "
+            + "    LIMIT 7 "
+            + ") AS subquery", nativeQuery = true)
+    Float getCurrentWeekLikeAvgById(@Param("id") Long influencerId);
+
+    @Query(value = "SELECT AVG(subquery.comments_cnt) "
+            + "FROM ( "
+            + "    SELECT m.comments_cnt "
+            + "    FROM Media m "
+            + "    WHERE m.influencer_id = :id "
+            + "    ORDER BY m.created_at DESC "
+            + "    LIMIT 7 "
+            + ") AS subquery", nativeQuery = true)
+    Float getCurrentWeekCommentsAvgById(@Param("id") Long influencerId);
+}
